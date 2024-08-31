f2f5190eb448cfce56554865993889f976d75a50
Luis Eduardo <banedycraft@gmail.com>
2024-08-30 21:01:38 -0300

    add questao3

---
+++ Questoes/src/Questao3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Questao3 {
+//  int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+	public static void main(String[] args) {
+		
+		
+		
+		int i = 12;
+		int soma = 0;
+		int k = 1;
+	while(k<i) {
+		k++;
+		soma = soma + k;
+		
+	}
+	System.out.println(soma);//77
+	}
+}
